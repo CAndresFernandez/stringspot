ad135e09ab10fe6f3c5ad4d23b6ee1d3c1321adc
Andres Fernandez <andres-carlos.fernandez-baker@oclock.school>
2023-12-06 21:01:17 +0100

    removed extraneous packages

--- frontend/stringspot/src/components/map.tsx
+++ frontend/stringspot/src/components/map.tsx
@@ -4,11 +4,10 @@ import "leaflet/dist/leaflet.css";
 
 const CentersMap = () => {
   const mapRef = useRef(null);
-  const parisLatitude = 48.864716;
-  const parisLongitude = 2.349014;
+  const parisPosition = [48.864716, 2.349014];
   return (
     <MapContainer
-      center={[parisLatitude, parisLongitude]}
+      center={parisPosition}
       zoom={13}
       ref={mapRef}
       style={{ height: "100%", width: "100%" }}
@@ -17,10 +16,8 @@ const CentersMap = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[48.864716, 2.349014]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
+      <Marker position={parisPosition}>
+        <Popup></Popup>
       </Marker>
     </MapContainer>
   );
