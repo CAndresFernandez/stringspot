645988a560c256d38e64a493d8953a9bc9654983
Andres Fernandez <andres-carlos.fernandez-baker@oclock.school>
2023-12-08 15:21:46 +0100

    lexik apilogin configured

--- backend/stringspot/config/bundles.php
+++ backend/stringspot/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
