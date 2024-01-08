795a940247cd02383f91402d61ba1abbe969c5b0
Andres Fernandez <andres-carlos.fernandez-baker@oclock.school>
2024-01-08 18:17:47 +0100

    userReducer updated to check for storeUser, profile and reservations pages updated to make requests, axios instance updated to refresh token when necessary

--- frontend/stringspot/src/api/axios.ts
+++ frontend/stringspot/src/api/axios.ts
@@ -1,4 +1,8 @@
 import axios from "axios";
+import {
+  getFromLocalStorage,
+  saveToLocalStorage,
+} from "../localStorage/localStorage";
 
 const myAxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -8,5 +12,36 @@ const myAxiosInstance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+myAxiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const storeUser = getFromLocalStorage("auth");
+        const refresh_token = storeUser.refresh_token;
+        const response = await myAxiosInstance.post("/token/refresh", {
+          refresh_token: refresh_token,
+        });
+
+        localStorage.removeItem("auth");
+        saveToLocalStorage("auth", {
+          refresh_token: response.data.refresh_token,
+          id: response.data.data.id,
+          first_name: response.data.data.first_name,
+          last_name: response.data.data.last_name,
+        });
+        return axios(originalRequest);
+      } catch (error) {
+        console.log("Error refreshing token:", error);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default myAxiosInstance;
--- frontend/stringspot/src/store/reducers/userReducer.ts
+++ frontend/stringspot/src/store/reducers/userReducer.ts
@@ -1,13 +1,13 @@
 import { createAction, createReducer } from "@reduxjs/toolkit";
-import myAxiosInstance from "../../api/axios";
-import { saveToLocalStorage } from "../../localStorage/localStorage";
-import { IPastRes } from "../../@types/user";
+import {
+  getFromLocalStorage,
+  saveToLocalStorage,
+} from "../../localStorage/localStorage";
 
 export interface RootState {
   id: number;
   logged?: boolean;
   email?: string;
-  token: null | string;
   refresh_token: null | string;
   //   roles?: [];
   first_name: string;
@@ -15,55 +15,43 @@ export interface RootState {
   //   pastRes: IPastRes[];
 }
 
+const storedUser = getFromLocalStorage("auth");
+
 export const initialState: RootState = {
-  id: 0,
-  logged: false,
+  id: storedUser ? storedUser.id : 0,
+  logged: !!storedUser,
   email: "",
-  token: null,
-  refresh_token: null,
-  first_name: "",
-  last_name: "",
-  //   roles: [],
-  //   pastRes: [],
+  refresh_token: storedUser ? storedUser.refresh_token : null,
+  first_name: storedUser ? storedUser.first_name : "",
+  last_name: storedUser ? storedUser.last_name : "",
 };
 
 export const getActionDisconnect = createAction("email/DISCONNECT");
 export const getActionLogin = createAction<{
-  //   token: string;
   refresh_token: string;
   id: number;
   first_name: string;
   last_name: string;
-  //   roles: [];
-  //   pastRes: [];
 }>("email");
 
 const userReducer = createReducer(initialState, (builder) => {
   builder
     .addCase(getActionLogin, (state, action) => {
       state.logged = true;
-      //   state.token = action.payload.token;
       state.refresh_token = action.payload.refresh_token;
       state.id = action.payload.id;
       state.first_name = action.payload.first_name;
       state.last_name = action.payload.last_name;
-      //   state.roles = action.payload.roles;
-      //   state.pastRes = action.payload.pastRes;
-      //   myAxiosInstance.defaults.headers.common.Authorization = `Bearer ${action.payload.token}`;
 
       saveToLocalStorage("auth", {
         refresh_token: action.payload.refresh_token,
-        // token: action.payload.token,
         id: action.payload.id,
         first_name: action.payload.first_name,
         last_name: action.payload.last_name,
-        // roles: action.payload.roles,
-        // pastRes: action.payload.pastRes,
       });
     })
     .addCase(getActionDisconnect, (state) => {
       state.logged = false;
-      //   delete myAxiosInstance.defaults.headers.common.Authorization;
       localStorage.removeItem("auth");
     });
 });
